af984e27906e8a1f201a12e3ab3a9e5bce89801c
hanxiao <1350690718@qq.com>
2018-07-13 16:17:39 +0800

    文章类别基础功能服务

---
+++ app/Services/TypeService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\DB;
+
+class TypeService{
+
+    public function create($data){
+        DB::table('types')->insert($data);
+    }
+    
+    public function isTypeNameExist(string $type_name){
+        $res = DB::table('types')->where('name',$type_name)->first();
+        return isTrue($res);
+    }
+
+    public function edit($type_id,$type_info){
+        DB::table('types')
+            ->where('id',$type_id)
+            ->update($type_info);
+    }
+
+    public function delete($type_id){
+        DB::transaction(function ()use ($type_id){
+            DB::table('types')
+                ->where('id',$type_id)
+                ->delete();
+            // 將该分类下的文章归类到未分类中
+            DB::table('articles')
+                ->where('type_id',$type_id)
+                ->update([
+                    'type_id'=>0
+                ]);
+        });
+
+    }
+}
