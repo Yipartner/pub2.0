d8dc82b6b949be630d0387223410c4ed99643e35
hanxiao <1350690718@qq.com>
2018-07-13 16:53:31 +0800

    标签与文章关系表

---
+++ database/migrations/2018_07_13_084707_create_article_tag_relations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTagRelationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('article_tag_relations',function (Blueprint $table){
+//            $table->increments('id');
+            $table->integer('tag_id');
+            $table->string('article_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('article_tag_relations');
+    }
+}
