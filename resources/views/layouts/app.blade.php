a456a40ecc0f0734ad426c92e55bfa3030a9548b
hanxiao <1350690718@qq.com>
2018-10-15 03:14:56 +0800

    去除访问量

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,7 +109,7 @@
     </nav>
     @yield('content')
     <div class="footer">
-        <div>
+        <div disabled>
             <span id="busuanzi_container_site_pv">
     本站总访问量<span id="busuanzi_value_site_pv"></span>次
 </span>
